646c0aea075fa26cb9c5e14a23cdb53ea3daa459
oliviahausle <oliviahausle@gmail.com>
2024-10-28 21:15:16 -0400

    Update streamlit_app.py
    getting fruity vice data to show data for chosen fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        
-    #st.write(ingredients_string)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use container_width=True)
+   
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
@@ -43,5 +44,4 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+
